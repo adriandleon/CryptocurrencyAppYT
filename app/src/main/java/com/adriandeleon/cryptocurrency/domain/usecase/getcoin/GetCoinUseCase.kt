6656105434bc1989ed225ccf7cacaa6e740ed61a
Dessiree Sánchez <dessireesanchez@gmail.com>
2021-11-03 22:10:44 -0300

    Add get coin use case

---
+++ app/src/main/java/com/adriandeleon/cryptocurrency/domain/usecase/getcoin/GetCoinUseCase.kt
@@ -0,0 +1,27 @@
+package com.adriandeleon.cryptocurrency.domain.usecase.getcoin
+
+import com.adriandeleon.cryptocurrency.common.Resource
+import com.adriandeleon.cryptocurrency.domain.model.CoinDetail
+import com.adriandeleon.cryptocurrency.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+
+    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coin = repository.getCoinById(coinId).toCoinDetail()
+            emit(Resource.Success(coin))
+        } catch (exception: HttpException) {
+            emit(Resource.Error(exception.localizedMessage ?: "Unexpected error occurred"))
+        } catch (exception: IOException) {
+            emit(Resource.Error("Couldn't reach server. Check your internet connection"))
+        }
+    }
+}
